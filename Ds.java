e3bb2417720fd6a2040e963e58bcd8b73562d9ce
HungLe1908 <125192441+HungLe1908@users.noreply.github.com>
2023-03-05 21:43:25 +0700

    Add files via upload

---
+++ Ds.java
@@ -0,0 +1,12 @@
+
+package Bt_lap4;
+
+import Bt_lap4.DanhSachSinhVien;
+public class Ds {
+    public static void main (String[] arg)
+    {
+    DanhSachSinhVien dssv = new DanhSachSinhVien();
+    dssv.menu();
+    
+    }
+}
